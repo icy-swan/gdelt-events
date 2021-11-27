c7bef0e15fd95a8f26f91d8706e432fb456e2f56
ianli <ian.primary.li@gmail.com>
2021-11-27 16:09:42 +0800

    feat: 一带一路数据

--- src/pages/TreeMapView/index.tsx
+++ src/pages/TreeMapView/index.tsx
@@ -3,123 +3,112 @@ import * as echarts from 'echarts'
 import originData from '../../config/originData';
 import countryData from '../../config/countryData';
 
+function getQueryString(name: string) {
+    let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    let r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return decodeURIComponent(r[2]);
+    };
+    return null;
+ }
+
+ function getLevelOption() {
+    return [
+      {
+        itemStyle: {
+          borderWidth: 0,
+          gapWidth: 5
+        }
+      },
+      {
+        itemStyle: {
+          gapWidth: 1
+        }
+      },
+      {
+        colorSaturation: [0.35, 0.5],
+        itemStyle: {
+          gapWidth: 1,
+          borderColorSaturation: 0.6
+        }
+      }
+    ];
+  }
+
 export default () => {
+    //处理数据
+    let targetData;
+    const type = getQueryString('type');
+    const typeCountryData = Object.assign({}, countryData[type]);
+
+    const measureData = originData['2020'];
+
+    if('isYDYL' === type) {
+        delete typeCountryData.all;
+        const regionNames = Object.keys(typeCountryData);
+        targetData = regionNames.map((name) => {
+            let value = 0;
+            const children = typeCountryData[name].map(cData => {
+                const {countryCode, countryName} = cData;
+                const cMeasureData = measureData[countryCode] || {RecordCount: 0};
+                if(!measureData[countryCode]) {
+                    console.log(countryCode, countryName);
+                }
+                const cValue = cMeasureData.RecordCount;
+                value += cValue;
+                return {
+                    value: cValue,
+                    name: countryName,
+                    path: name,
+                }
+            })
+            return {
+                value,
+                name,
+                path: name,
+                children,
+            }
+        });
+    }
+
+
     useEffect(()=> {
         const chartDom = document.getElementById('main');
         const myChart = echarts.init(chartDom, null, {
             renderer: 'svg'
         });
         let option;
-        const targetData = originData[2020];
-        let series = [];
-        let regionsList = ['北美洲', '亚洲', '欧洲', '大洋洲', '非洲', '南美洲'];
-
-        regionsList.forEach(k => {
-            const countryData = targetData[k];
-            const countryNames = Object.keys(countryData);
-            const data = countryNames.map(c => {
-                const countryObj = countryData[c];
-                return [countryObj.count, countryObj.risk1, countryObj.countryCN];
-            })
-            series.push({
-                name: k,
-                type: 'scatter',
-                itemStyle: {
-                    opacity: 0.8,
-                    shadowOffsetX: 0,
-                    shadowOffsetY: 0,
-                    shadowColor: 'rgba(0,0,0,0.3)'
-                },
-                data,
-                symbolSize: 30,
-                label: {
-                    show: true,
-                    formatter: (params)=> {
-                        // debugger
-                        return params.data[2]
-                    },
-                    position: 'top',
-                    // width: 50,
-                    // overflow: 'truncate',
-                    // fontSize: 16
-                },
-                // markPoint: {
-                //     data: [
-                //         // { type: 'max', name: 'Max', valueIndex: 0 },
-                //         // { type: 'min', name: 'Min', valueIndex: 0 },
-                //         { type: 'max', name: 'Max', valueIndex: 1 },
-                //         { type: 'min', name: 'Min', valueIndex: 1 }
-                //     ]
-                // }
-            });
-        })
         myChart.setOption(
             (option = {
-                color: ['#3498DB', '#E67E22', '#27AE60', '#9B59B6', '#F1C40F', '#dd2727', '#34495E', '#E74C3C', '#8E44AD', '#7F8C8D'],
-                toolbox: {
+              color: ['#3498DB', '#E67E22', '#27AE60', '#9B59B6', '#F1C40F', '#dd2727', '#34495E', '#E74C3C', '#8E44AD', '#7F8C8D'],
+              toolbox: {
+                show: true,
+                orient: 'vertical',
+                left: 'right',
+                top: 'center',
+                feature: {
+                  saveAsImage: { show: true }
+                }
+              },
+              series: [
+                {
+                  name: '2020',
+                  type: 'treemap',
+                  visibleMin: 300,
+                  label: {
                     show: true,
-                    orient: 'vertical',
-                    left: 'right',
-                    top: 'center',
-                    feature: {
-                        mark: { show: true },
-                        dataView: { show: true, readOnly: false },
-                        magicType: { show: true, type: ['line', 'bar', 'stack'] },
-                        restore: { show: true },
-                        saveAsImage: { show: true }
-                    }
-                },
-                legend: {
-                    top: 10,
-                    data: regionsList,
-                    textStyle: {
-                        fontSize: 16
-                    }
-                },
-                grid: {
-                    left: '10%',
-                    right: '10%',
-                    top: '18%',
-                    bottom: '10%'
-                },
-                xAxis: {
-                    type: 'log',
-                    name: '风险量',
-                    nameGap: 16,
-                    nameTextStyle: {
-                        fontSize: 16
-                    },
-                    splitLine: {
-                        show: false
-                    }
-                },
-                yAxis: {
-                    type: 'value',
-                    name: '变化率',
-                    nameLocation: 'end',
-                    nameGap: 20,
-                    splitNumber: 10,
-                    nameTextStyle: {
-                        fontSize: 16
-                    },
-                    axisLabel: {
-                        formatter: function (value, index) {
-                            return value * 100 + '%';
-                        }
-                    },
-                    splitLine: {
-                        show: false
-                    }
-                },
-                tooltip: {
-                    formatter: function (param) {
-                        var value = param.value;
-                        return value[2] + ' : 数量' + value[0] + ' 同比' + (parseFloat(value[1] * 100)).toFixed(2) + '%';
-                    }
-                },
-                series,
+                    formatter: '{b}',
+                    fontSize: 20,
+                  },
+                  levels: getLevelOption(),
+                  data: targetData,
+                  breadcrumb: {
+                    show: false
+                  }
+                }
+              ]
             })
-        );
+          );
 
         option && myChart.setOption(option);
 
