df69814a036a44bf5cbba4ab722f2ec37c4f291e
李寻欢 <ian.primary.li@gmail.com>
2022-01-08 14:21:51 +0800

    feat: 开始做玫瑰图

--- src/pages/RoseView/index.tsx
+++ src/pages/RoseView/index.tsx
@@ -12,46 +12,52 @@ export default () => {
         let option;
         myChart.setOption(
             (option = option = {
-                color: ['#3498DB', '#E67E22', '#27AE60', '#9B59B6', '#F1C40F', '#dd2727', '#34495E', '#E74C3C', '#8E44AD', '#7F8C8D'],
+                color: colors,
                 legend: {
-                  orient: 'vertical',
-                  icon: 'rect',
-                  textStyle: {
-                      fontFamily: 'serif'
-                  }
+                    orient: 'vertical',
+                    icon: 'rect',
+                    textStyle: {
+                        fontFamily: 'serif'
+                    }
                 },
                 toolbox: {
-                  show: true,
-                  feature: {
-                    mark: { show: true },
-                    dataView: { show: true, readOnly: false },
-                    restore: { show: true },
-                    saveAsImage: { show: true }
-                  }
+                    show: true,
+                    feature: {
+                        mark: { show: true },
+                        dataView: { show: true, readOnly: false },
+                        restore: { show: true },
+                        saveAsImage: { show: true }
+                    }
                 },
                 series: [
-                  {
-                    name: 'Nightingale Chart',
-                    type: 'pie',
-                    radius: [50, 250],
-                    center: ['50%', '50%'],
-                    roseType: 'area',
-                    itemStyle: {
-                      borderRadius: 0
-                    },
-                    label: {
-                      position: 'inside'
-                    },
-                    data: [
-                      { value: 4136, name: '合作沟通摩擦' },
-                      { value: 2632, name: '政局变更摩擦' },
-                      { value: 8631, name: '战争武装摩擦' },
-                      { value: 6749, name: '司法治安摩擦' },
-                      { value: 3209, name: '经济财产摩擦' },
-                    ]
-                  }
+                    {
+                        name: 'Nightingale Chart',
+                        type: 'pie',
+                        radius: [50, 250],
+                        center: ['50%', '50%'],
+                        roseType: 'area',
+                        itemStyle: {
+                            borderRadius: 0
+                        },
+                        label: {
+                            position: 'inside',
+                            textStyle: {
+                                fontFamily: 'serif',
+                            },
+                            // color: '#fff',
+                            // textBorderColor: '#333',
+                            // textBorderWidth: 1
+                        },
+                        data: [
+                            { value: 4136, name: '合作沟通摩擦' },
+                            { value: 2632, name: '政局变更摩擦' },
+                            { value: 8631, name: '战争武装摩擦' },
+                            { value: 6749, name: '司法治安摩擦' },
+                            { value: 3209, name: '经济财产摩擦' },
+                        ]
+                    }
                 ]
-              })
+            })
         );
 
         option && myChart.setOption(option);
