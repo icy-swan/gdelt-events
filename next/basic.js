f0dd3c21771ecedf10bb1919c3da6fe96281cb11
ianli <ian.primary.li@gmail.com>
2022-05-28 22:20:11 +0800

    一些原型的内容

--- next/basic.js
+++ next/basic.js
@@ -48,6 +48,34 @@ kiwi.notGood.push(kiwi.cName);
 cat.prototype.sayHello = function() {return 'cat hello'};
 /* console.log(animal.prototype.sayHello) */
 
+function C() {}
+function D() {}
+
+let o = new C()
+
+// true, because: Object.getPrototypeOf(o) === C.prototype
+o instanceof C
+
+// false, because D.prototype is nowhere in o's prototype chain
+o instanceof D
+
+o instanceof Object           // true, because:
+C.prototype instanceof Object // true
+
+C.prototype = {}
+let o2 = new C()
+
+o2 instanceof C  // true
+
+// false, because C.prototype is nowhere in
+// o's prototype chain anymore
+o instanceof C
+
+D.prototype = new C()  // add C to [[Prototype]] linkage of D
+let o3 = new D()
+o3 instanceof D        // true
+o3 instanceof C        // true since C.prototype is now in o3's prototype chain
+
 // 简单去重
 const arr = new Array();
 let count = 100;
