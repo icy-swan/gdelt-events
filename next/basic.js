0f26124b7be32a1455818f7c985135fba08d87a1
ianli <ian.primary.li@gmail.com>
2022-06-14 21:40:02 +0800

    feat: 高级去重下的俄乌克兰数据

--- next/basic.js
+++ next/basic.js
@@ -1,89 +1,91 @@
 function animal(name) {
-    this.name = name;
-    this.notGood = []
+  this.name = name;
+  this.notGood = [];
 }
-animal.prototype.sing = function(){
-    return `${this.name} + sing`
-}
+animal.prototype.sing = function () {
+  return `${this.name} + sing`;
+};
 // fast extend
 function dog(dName) {
-  	this.dName = dName;
+  this.dName = dName;
 }
-dog.prototype = new animal('dog');
-const dei = new dog('dei');
-const dfe = new dog('dfe');
+dog.prototype = new animal("dog");
+const dei = new dog("dei");
+const dfe = new dog("dfe");
 dei.notGood.push(dei.dName);
 /* console.log(dfe.notGood); */
 
 // override ownproperty
 function bird(bName) {
-    this.bName = bName;
-    animal.call(this, 'bird');
+  this.bName = bName;
+  animal.call(this, "bird");
 }
 
 // ownProperty & prototype
 function snake(sName) {
-		this.sName = sName;
-    animal.call(this, 'snake');
+  this.sName = sName;
+  animal.call(this, "snake");
 }
 /* snake.prototype = animal.prototype; */
-snake.prototype = new animal('snake');
+snake.prototype = new animal("snake");
 
 // best extend
 function cat(cName) {
-    this.cName = cName;
-    animal.call(this, 'cat');
+  this.cName = cName;
+  animal.call(this, "cat");
 }
 function temp() {}
 temp.prototype = animal.prototype;
 cat.prototype = new temp();
 cat.prototype.constructor = cat;
 
-const kiwi = new cat('kiwi')
-const waga = new cat('waga')
+const kiwi = new cat("kiwi");
+const waga = new cat("waga");
 
 kiwi.notGood.push(kiwi.cName);
 /* console.log(waga.notGood); */
 
-cat.prototype.sayHello = function() {return 'cat hello'};
+cat.prototype.sayHello = function () {
+  return "cat hello";
+};
 /* console.log(animal.prototype.sayHello) */
 
 function C() {}
 function D() {}
 
-let o = new C()
+let o = new C();
 
 // true, because: Object.getPrototypeOf(o) === C.prototype
-o instanceof C
+o instanceof C;
 
 // false, because D.prototype is nowhere in o's prototype chain
-o instanceof D
+o instanceof D;
 
-o instanceof Object           // true, because:
-C.prototype instanceof Object // true
+o instanceof Object; // true, because:
+C.prototype instanceof Object; // true
 
-C.prototype = {}
-let o2 = new C()
+C.prototype = {};
+let o2 = new C();
 
-o2 instanceof C  // true
+o2 instanceof C; // true
 
 // false, because C.prototype is nowhere in
 // o's prototype chain anymore
-o instanceof C
+o instanceof C;
 
-D.prototype = new C()  // add C to [[Prototype]] linkage of D
-let o3 = new D()
-o3 instanceof D        // true
-o3 instanceof C        // true since C.prototype is now in o3's prototype chain
+D.prototype = new C(); // add C to [[Prototype]] linkage of D
+let o3 = new D();
+o3 instanceof D; // true
+o3 instanceof C; // true since C.prototype is now in o3's prototype chain
 
 // 简单去重
 const arr = new Array();
 let count = 100;
 while (count > 0) {
-    arr.push(Math.floor(Math.random() * 100));
-    count -= 1;
+  arr.push(Math.floor(Math.random() * 100));
+  count -= 1;
 }
-arr.push(NaN, NaN, undefined, undefined, {a:1});
+arr.push(NaN, NaN, undefined, undefined, { a: 1 });
 /* console.log(arr.length) */
 const uniArr = new Set(arr);
 /* console.log(Array.from(uniArr).length); */
@@ -93,76 +95,83 @@ const uniArr1 = [...new Set(arr)];
 
 // nan, object, function都能解决
 const tempMap = new Map();
-arr.forEach(item=> {
-    tempMap.set(item, undefined);
-})
+arr.forEach((item) => {
+  tempMap.set(item, undefined);
+});
 const uniArr2 = tempMap.keys();
 /* console.log(Array.from(uniArr2).length); */
 
 const uniArr3 = arr.reduce((last, current) => {
-	return last.includes(current) ? last : last.concat(current);
-}, [])
+  return last.includes(current) ? last : last.concat(current);
+}, []);
 /* console.log(uniArr3.length); */
 
 // 简单的发布订阅
 class Notifier {
-    constructor() {
-        this.events = new Map();
+  constructor() {
+    this.events = new Map();
+  }
+  subscribe(eventName, callback) {
+    const callbackList = this.events.get(eventName);
+    this.events.set(
+      eventName,
+      typeof callbackList !== "undefined"
+        ? callbackList.concat(callback)
+        : [callback]
+    );
+  }
+  publish(eventName, ...args) {
+    const callbackList = this.events.get(eventName);
+    if (Array.isArray(callbackList) && callbackList.length > 0) {
+      this.events.get(eventName).forEach((cb) => cb(...args));
     }
-    subscribe(eventName, callback) {
-        const callbackList = this.events.get(eventName);
-        this.events.set(eventName, typeof callbackList !== 'undefined' ? callbackList.concat(callback) : [callback]);
+  }
+  unsubscribe(eventName, callback) {
+    const callbackList = this.events.get(eventName);
+    if (Array.isArray(callbackList) && callbackList.length > 0) {
+      this.events.set(
+        eventName,
+        callbackList.filter((cb) => cb !== callback)
+      );
     }
-    publish(eventName, ...args) {
-        const callbackList = this.events.get(eventName);
-        if(Array.isArray(callbackList) && callbackList.length > 0) {
-            this.events.get(eventName).forEach(cb => cb(...args));
-        }
-    }
-    unsubscribe(eventName, callback) {
-        const callbackList = this.events.get(eventName);
-        if(Array.isArray(callbackList) && callbackList.length > 0) {
-            this.events.set(eventName, callbackList.filter(cb => cb !== callback));
-        }
-    }
-    
+  }
 }
 const nf = new Notifier();
 /* function showLoad(data) {
     console.log('load', data);
 } */
-nf.subscribe('load', showLoad);
-nf.subscribe('load', showLoad);
-nf.publish('load', 'a information');
-nf.unsubscribe('load', showLoad);
-nf.publish('load', 'a information');
+nf.subscribe("load", showLoad);
+nf.subscribe("load", showLoad);
+nf.publish("load", "a information");
+nf.unsubscribe("load", showLoad);
+nf.publish("load", "a information");
 
 // memory leak
 const o1 = {
-    a: 1,
-    get a() {
-        return this.a;
-    },
-    set a(x) {
-        this.a = x;
-    }
-}
+  a: 1,
+  get a() {
+    return this.a;
+  },
+  set a(x) {
+    this.a = x;
+  },
+};
 // o1.a
 // async
 // 宏任务微任务
 // asyc 和 promise一样
 async function async1() {
-    console.log('async1 start'); // second
-    await async2(); // third
-    console.log('async1 end'); // 6th
+  console.log("async1 start"); // second
+  await async2(); // third
+  console.log("async1 end"); // 6th
 }
 async function async2() {
-    console.log('async2') // third
+  console.log("async2"); // third
 }
-console.log('script start'); // first
-setTimeout(()=> {
-    console.log('timeout'); // 8th or 10th
-}, 0)
+console.log("script start"); // first
+setTimeout(() => {
+  console.log("timeout"); // 8th or 10th
+}, 0);
 /* setimmediate(()=> {
     console.log('imm'); //9th
 }) */
@@ -170,16 +179,18 @@ async1(); // second
 /* process.nextTicker(()=> {
     console.log('next ticker'); //8th
 }) */
-new Promise((resolve, reject)=> {
-    console.log('promise1'); // 4th
-    resolve();
-    throw new Error('e0');// no response
-}).then(()=> {
-    console.log('promise2'); // 7th
-}).catch(e=> {
-    console.log(e);
+new Promise((resolve, reject) => {
+  console.log("promise1"); // 4th
+  resolve();
+  throw new Error("e0"); // no response
 })
-console.log('script end'); // 5th
+  .then(() => {
+    console.log("promise2"); // 7th
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+console.log("script end"); // 5th
 
 // max
 Math.max() === -Infinity;
@@ -192,72 +203,80 @@ Math.min() === Infinity;
 const promise1 = Promise.resolve(3);
 const promise2 = 42;
 const promise3 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 100, 'foo');
+  setTimeout(resolve, 100, "foo");
 });
 
-Promise.all([promise1, promise2, promise3]).then((values) => {
-  console.log(values);
-}, resaon => {console.log(resaon)});
+Promise.all([promise1, promise2, promise3]).then(
+  (values) => {
+    console.log(values);
+  },
+  (resaon) => {
+    console.log(resaon);
+  }
+);
 // allSettled  -- all resolve or reject
 // any -- any resolve
 // race -- first resolve or reject
-for(let i =0; i < 10; i++) {
-    setTimeout((i)=> {
-        console.log(i)
-    }, 1000)
+for (let i = 0; i < 10; i++) {
+  setTimeout((i) => {
+    console.log(i);
+  }, 1000);
 }
-for (let i=0; i<5; i++) {
-    setTimeout(() => console.log(i), 1000*i)
+for (let i = 0; i < 5; i++) {
+  setTimeout(() => console.log(i), 1000 * i);
 }
-const sleep = ()=>new Promise(res => setTimeout(res, 1000));
+const sleep = () => new Promise((res) => setTimeout(res, 1000));
 (async function it() {
-    for(let i =0; i < 5; i++) {
-        await sleep();
-        console.log(i);
-    }
+  for (let i = 0; i < 5; i++) {
+    await sleep();
+    console.log(i);
+  }
 })();
 
 // about this
 
 // call bind apply
 function Person(name, age) {
-    this.name = name;
-    this.age = age;
+  this.name = name;
+  this.age = age;
 }
 function Male(name, age) {
-    this.type = 'male';
-    Person.call(this, name, age);
+  this.type = "male";
+  Person.call(this, name, age);
 }
-function temp() {};
+function temp() {}
 temp.prototype = Person.prototype;
 Male.prototype = new temp();
 Male.prototype.constructor = Male;
 // bind
 const arrT1 = [1, 3, 5];
-const max = Math.max.apply(Math, arrT1);// max不接受数组 当然可以用...arrT1
+const max = Math.max.apply(Math, arrT1); // max不接受数组 当然可以用...arrT1
 console.log(max);
 // apply
 function a() {
-    this.age = 3;
-    this.log = function() {
-        setTimeout(function() {
-            console.log(this);
-            console.log(this.age);
-        }.bind(this), 0);
-    }
+  this.age = 3;
+  this.log = function () {
+    setTimeout(
+      function () {
+        console.log(this);
+        console.log(this.age);
+      }.bind(this),
+      0
+    );
+  };
 }
 // 等价于箭头函数，箭头函数的作用域是定义时确定，绑定到父级的块
 function a() {
-    this.age = 3;
-    this.log = function() {
-        setTimeout(() => {
-            console.log(this);
-            console.log(this.age);
-        }, 0);
-    }
+  this.age = 3;
+  this.log = function () {
+    setTimeout(() => {
+      console.log(this);
+      console.log(this.age);
+    }, 0);
+  };
 }
 
-new a().log()// a{age:3, log: f}, 3
+new a().log(); // a{age:3, log: f}, 3
 
 // 箭头函数不创建自身的this、没原型即没costructor，argument等，无法作为构造函数
 
@@ -278,5 +297,29 @@ function newFun(cont, ...args) {
 }
 const test1 = newFun(TestObj,1) */
 
-const s1 = Symbol('a');
-Symbol.for('a') !== Symbol('b')
+const s1 = Symbol("a");
+Symbol.for("a") !== Symbol("b");
+
+function jsonp({ url, query }) {
+  let script = document.createElement("script");
+  let cb = `jsonpCallBack${new Date().getTime()}${Math.floor(
+    Math.random(5) * 100000
+  )}`;
+  let params = { ...query, cb };
+
+  let arr = [];
+  for (let key in params) {
+    arr.push(`${key}=${params[key]}`);
+  }
+
+  script.src = `${url}?${arr.join("&")}`;
+  document.body.appendChild(script);
+
+  return new Promise((resolve, rej) => {
+    window[cb] = function (res) {
+      document.body.removeChild(script);
+      delete window[cb];
+      resolve(res);
+    };
+  });
+}
---
+++ sql/20220614/UKR&RSU-仅看subevent为3.sql
@@ -0,0 +1,113 @@
+WITH query AS (
+    SELECT
+        GLOBALEVENTID,
+        MonthYear,
+        QuadClass,
+        CASE
+            WHEN Actor1CountryCode = 'RUS' THEN Actor1CountryCode
+            ELSE Actor2CountryCode
+        END AS Actor1CountryCode,
+        CASE
+            WHEN Actor2CountryCode = 'RUS' THEN Actor1CountryCode
+            ELSE Actor2CountryCode
+        END AS Actor2CountryCode,
+        3 AS SubEventType,
+        GoldsteinScale,
+        SOURCEURL,
+    FROM
+        `gdelt-bq.full.events`
+    WHERE
+        YEAR >= 2010
+        AND GoldsteinScale < 0 -- AND AvgTone < 0
+        AND (
+            (
+                Actor1CountryCode = 'RUS'
+                AND Actor2CountryCode = 'UKR'
+            )
+            OR (
+                Actor2CountryCode = 'RUS'
+                AND Actor1CountryCode = 'UKR'
+            )
+        )
+        AND EventCode IN (
+            '0256',
+            '093',
+            '094',
+            '1012',
+            '1032',
+            '1034',
+            '1056',
+            '1123',
+            '1124',
+            '1125',
+            '1212',
+            '1222',
+            '1224',
+            '1246',
+            '137',
+            '138',
+            '1381',
+            '1382',
+            '1383',
+            '1384',
+            '1385',
+            '139',
+            '150',
+            '152',
+            '1622',
+            '175',
+            '180',
+            '190',
+            '191',
+            '192',
+            '193',
+            '194',
+            '195',
+            '1951',
+            '1952',
+            '196',
+            '200',
+            '201',
+            '202',
+            '203',
+            '204',
+            '2041',
+            '2042'
+        )
+)
+SELECT
+    *
+FROM
+    query AS A
+WHERE
+    EXISTS(
+        SELECT
+            1
+        FROM
+            (
+                SELECT
+                    ARRAY_AGG(
+                        DISTINCT (
+                            CASE
+                                WHEN AC IN ('TWN', 'HKG', 'MAC')
+                                OR AC IS NULL THEN 'CHN'
+                                ELSE AC
+                            END
+                        )
+                    ) AS AC_agg,
+                    SOURCEURL
+                FROM
+                    `gdelt-bq.full.events` UNPIVOT(
+                        AC FOR type IN(Actor1CountryCode, Actor2CountryCode)
+                    )
+                WHERE
+                    YEAR >= 2010
+                GROUP BY
+                    SOURCEURL
+            ) AS T
+        WHERE
+            A.Actor1CountryCode IN UNNEST(T.AC_agg)
+            AND A.Actor2CountryCode IN UNNEST(T.AC_agg)
+            AND ARRAY_LENGTH(AC_agg) <= 2
+            AND A.SOURCEURL = T.SOURCEURL
+    )
