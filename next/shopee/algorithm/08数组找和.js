ebee3a526bed9d01e47e620f5b676f52493be6c3
ianli <ian.primary.li@gmail.com>
2022-08-07 23:20:38 +0800

    feat: 修复bug

--- next/shopee/algorithm/08数组找和.js
+++ next/shopee/algorithm/08数组找和.js
@@ -1,22 +1,27 @@
 const ca = [10, 1, 2, 7, 6, 1, 5],
   tar = 8;
 const getCombin = (ca = [], tar = 0) => {
+  const savedMap = new Map();
   var res = [];
   ca = ca.sort((a, b) => a - b);
-  function dfs(i, sum, ans) {
+  function searchTree(i, sum, ans) {
     if (sum > tar) return;
     if (sum == tar) {
-      res.push([...ans]);
+      if(!savedMap.has(ans)) {
+        res.push([...ans]);
+      }
+      savedMap.set(ans, true);
     }
     for (i; i < ca.length; i++) {
       sum += ca[i];
       ans.push(ca[i]);
-      dfs(i + 1, sum, ans);
+      searchTree(i + 1, sum, ans);
       sum -= ca[i];
       ans.pop();
-      while (ca[i + 1] == ca[i]) i++;
+    //   while (ca[i + 1] == ca[i]) i++;
     }
   }
-  dfs(0, 0, []);
+  searchTree(0, 0, []);
   return res;
 };
+console.log(getCombin(ca, tar));
