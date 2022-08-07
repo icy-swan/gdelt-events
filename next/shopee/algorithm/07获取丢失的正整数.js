c1b2cc51277d21796347338af0bc1bfdaed80963
ianli <ian.primary.li@gmail.com>
2022-08-07 22:56:53 +0800

    feat: 高级

---
+++ next/shopee/algorithm/07获取丢失的正整数.js
@@ -0,0 +1,15 @@
+const nums = [3, 4, -1, 1];
+const getMiss = (nums) => {
+    for(let n of nums) {
+        if(typeof nums[+n - 1] !== 'undefined') {//将整数对应位置设置为字符串
+            nums[+n -1 ] = '' + nums[+n -1 ];
+        }
+    }
+    for(let i = 0; i < nums.length; i++) {
+        if(typeof nums[i] === 'number') {//遍历，如果找到非字符串 - 就是该整数没出现
+            return i + 1;
+        }
+    }
+    return nums.length + 1;
+}
+console.log(getMiss(nums));
